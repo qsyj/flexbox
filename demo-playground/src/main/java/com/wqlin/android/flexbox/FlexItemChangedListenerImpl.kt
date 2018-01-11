03a639515f832e07f5017ea16cde11d94465cb17
wangql <wangql@leleyuntech.com>
2018-01-11 17:45:38 +0800

    在google/flexbox-layout基础上 1.FlexContainer增加isExpand()方法-->line行是否展开(前提设置了FlexItem.flexGrow); 2.FlexboxLayoutManager FlexboxLayout增加setLastLineExpand()-->最后一行的item总宽度与父View的宽度比例来达到多大比例时才展开

---
+++ demo-playground/src/main/java/com/wqlin/android/flexbox/FlexItemChangedListenerImpl.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wqlin.android.flexbox
+
+import android.view.ViewGroup
+import com.wqlin.widget.flexbox.FlexContainer
+import com.wqlin.widget.flexbox.FlexItem
+
+/**
+ * Default implementation for the [FlexItemChangedListener].
+ */
+internal class FlexItemChangedListenerImpl(private val flexContainer: FlexContainer) : FlexItemChangedListener {
+
+    override fun onFlexItemChanged(flexItem: FlexItem, viewIndex: Int) {
+        val view = flexContainer.getFlexItemAt(viewIndex)
+        view.layoutParams = flexItem as ViewGroup.LayoutParams
+    }
+}
